9a52c917c84603b2d2509dc3daa367f2387a07f5
Guido Lagar <guidolagar@gmail.com>
2023-03-07 17:50:26 -0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,9 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+# Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  Article.create(title: Faker::Music.band, content: Faker::Sports::Football.player)
+end
